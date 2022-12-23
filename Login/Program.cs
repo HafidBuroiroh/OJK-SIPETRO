4668c6a8e75749e47ce9258909951dab639efd7f
HafidBuroiroh <hafidburoiroh09@gmail.com>
2022-12-23 13:35:49 +0700

    ekstension

--- Login/Program.cs
+++ Login/Program.cs
@@ -1,7 +1,22 @@
+using Login.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 var app = builder.Build();
 
@@ -11,6 +26,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
@@ -24,19 +44,7 @@ app.MapControllerRoute(name: "Home",
                 pattern: "home/{*index}",
                 defaults: new { controller = "Home", action = "Index" });
 
-app.MapControllerRoute(name: "Transfer_Data",
-                pattern: "transfer/{*index}",
-                defaults: new { controller = "Transfer", action = "Index" });
-
-app.MapControllerRoute(name: "Generate_Data",
-                pattern: "generate/{*index}",
-                defaults: new { controller = "Generate", action = "Index" });
-
-app.MapControllerRoute(name: "Send_Data",
-                pattern: "send/{*index}",
-                defaults: new { controller = "Send", action = "Index" });
-
-app.MapControllerRoute(name: "default",
+ControllerActionEndpointConventionBuilder controllerActionEndpointConventionBuilder = app.MapControllerRoute(name: "default",
                pattern: "{controller=Login}/{action=Login}/{id?}");
 
 
